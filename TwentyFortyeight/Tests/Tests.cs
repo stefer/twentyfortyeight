57e3607631b3b59f5b790d5e0860cf72a81ab74c
Stefan Eriksson <stefan.piaostefan@gmail.com>
2017-01-22 21:20:32 +0100

    First version

---
+++ TwentyFortyeight/Tests/Tests.cs
@@ -0,0 +1,115 @@
+﻿namespace Tests
+{
+    using System;
+    using System.Linq;
+    using TewntyFortyeight;
+    using Xunit;
+
+
+    public class Tests
+    {
+
+        [Fact]
+        public void NewBoard_IsEmpty() 
+        {
+            Board board = new Board();
+
+            Assert.Equal(Board.Empty.Cells, board.Cells);
+        }
+
+        [Fact]
+        public void EmptyBoard_HasOnlyZeroValues()
+        {
+            Assert.All(
+               Board.Empty.Cells, r => 
+                    Assert.True(Array.TrueForAll(r, c => c == 0)));
+        }
+
+        [Fact]
+        public void Start_SetTwoValues()
+        {
+            Board board = new Board();
+            board.Start();
+
+            Assert.Equal(2, board.AllCells.Count(x => x > 0));
+            Assert.All(board.AllCells, c => Assert.True(c == 0 || c == 2 || c == 4));
+        }
+
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(1, 1)]
+        [InlineData(2, 2)]
+        [InlineData(3, 3)]
+        [InlineData(4, 4)]
+        [InlineData(5, 5)]
+        [InlineData(6, 6)]
+        public void Right_ShouldShiftAllToRightEdge(int r, int c)
+        {
+            Board board = new Board();
+            board.Set(r, c, 2);
+            board.Right();
+
+            Assert.Equal(0, board.Get(r, c));
+            Assert.Equal(2, board.Get(r, Board.Last));
+        }
+
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(1, 1)]
+        [InlineData(2, 2)]
+        [InlineData(3, 3)]
+        [InlineData(4, 4)]
+        [InlineData(5, 5)]
+        [InlineData(6, 6)]
+        public void Right_ShouldShiftAllToRightEdge_AndAdd(int r, int c)
+        {
+            Board board = new Board();
+            board.Set(r, Board.Last, 2);
+            board.Set(r, c, 2);
+            board.Right();
+
+            Assert.Equal(0, board.Get(r, c));
+            Assert.Equal(4, board.Get(r, Board.Last));
+        }
+
+        [Fact]
+        public void Right_ShouldAddIfAllSet()
+        {
+            Board board = new Board();
+            board.Set(0, new int[] { 0, 2, 0, 2, 0, 2, 0, 2});
+            board.Right();
+            
+            Assert.Equal(new int[] { 0, 0, 0, 0, 0, 0, 0, 8 }, board.Get(0));
+        }
+
+
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(2, 2)]
+        [InlineData(3, 3)]
+        [InlineData(4, 4)]
+        [InlineData(5, 5)]
+        [InlineData(6, 6)]
+        [InlineData(7, 7)]
+        public void Left_ShouldShiftAllToLeftEdge_AndAdd(int r, int c)
+        {
+            Board board = new Board();
+            board.Set(r, Board.First, 2);
+            board.Set(r, c, 2);
+            board.Left();
+
+            Assert.Equal(0, board.Get(r, c));
+            Assert.Equal(4, board.Get(r, Board.First));
+        }
+
+        [Fact]
+        public void Left_ShouldAddIfAllSet()
+        {
+            Board board = new Board();
+            board.Set(0, new int[] { 0, 2, 0, 2, 0, 2, 0, 2 });
+            board.Left();
+
+            Assert.Equal(new int[] { 8, 0, 0, 0, 0, 0, 0, 0 }, board.Get(0));
+        }
+    }
+}
---
+++ TwentyFortyeight/src/Lib/Board.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TewntyFortyeight
+{
+    public class Board
+    {
+        public const int Size = 8;
+        public const int Last = Size - 1;
+        public const int First = 0;
+
+        public static Board Empty = new Board();
+        private readonly Random rand = new Random();
+
+        private readonly int[][] cells = new int[Size][]
+            { new int[Size], new int[Size] , new int[Size] , new int[Size], new int[Size], new int[Size],  new int[Size], new int[Size] };
+
+        public int[][] Cells
+        {
+            get { return cells; }
+        }
+
+        public IEnumerable<int> AllCells
+        {
+            get
+            {
+                for (int ri = 0; ri < cells.Length; ri++)
+                {
+                    int[] row = Get(ri);
+                    for (int ci = 0; ci < row.Length; ci++)
+                    {
+                        yield return row[ci];
+                    }
+                }
+            }
+        }
+
+        public void Start()
+        {
+            Set(rand.Next(0, Size), rand.Next(0, Size), rand.Next(1, 3) * 2);
+            Set(rand.Next(0, Size), rand.Next(0, Size), rand.Next(1, 3) * 2);
+        }
+
+        public void Set(int r, int c, int value)
+        {
+            cells[r][c] = value;
+        }
+
+        public void Set(int r, int[] values)
+        {
+            if (values.Length != Size) throw new ArgumentException("Invalid array length", nameof(values));
+
+            cells[r] = values;
+        }
+
+        public int Get(int r, int c)
+        {
+            return cells[r][c];
+        }
+
+        public int[] Get(int i)
+        {
+            return Cells[i];
+        }
+
+        public void Right()
+        {
+            for (int r = 0; r < Size; r++)
+            {
+                for (int c = Size - 2; c >= 0; c--)
+                {
+                    var current = Get(r, c);
+                    for (int i = c + 1; i < Size; i++)
+                    {
+                        var candidate = Get(r, i);
+                        if (candidate == 0 || candidate == current)
+                        {
+                            current += candidate;
+                            Set(r, i, current);
+                            Set(r, i - 1, 0);
+                        }
+                    }
+                }
+            }
+        }
+
+        public void Left()
+        {
+            for (int r = First; r < Size; r++)
+            {
+                for (int c = First + 1; c < Size; c++)
+                {
+                    var current = Get(r, c);
+                    for (int i = c - 1; i >= First; i--)
+                    {
+                        var candidate = Get(r, i);
+                        if (candidate == 0 || candidate == current)
+                        {
+                            current += candidate;
+                            Set(r, i, current);
+                            Set(r, i + 1, 0);
+                        }
+                    }
+                }
+            }
+        }
+
+
+        public override string ToString()
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+
+            sb.AppendLine(" -------------------------------------------------------------");
+            for (int r = 0; r < Size; r++)
+            {
+                for (int c = 0; c < Size; c++)
+                {
+                    sb.AppendFormat(" | {0,5:0}", cells[r][c]);
+                }
+                sb.AppendLine(" |");
+            }
+            sb.AppendLine(" -------------------------------------------------------------");
+            return sb.ToString();
+        }
+
+    }
+}
